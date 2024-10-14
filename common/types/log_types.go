0c30bb3ec96ec49b8b0b41dfa4e4a3b949892c16
José Lico <lico.jose.pt@gmail.com>
2024-10-14 17:44:08 +0100

    Small fixes

--- common/types/log_types.go
+++ common/types/log_types.go
@@ -6,7 +6,7 @@ package types
 // 	"timestamp": "2024-10-03T15:20:30Z",
 // 	"level": "INFO",
 // 	"message": "User logged in",
-// 	"userId": "12345",
+// 	"userId": "97a569df-498c-49a3-9676-ec82b992dc07",
 // 	"additionalData": {
 // 	  "ipAddress": "192.168.1.1",
 // 	  "sessionId": "abcde12345"
@@ -17,7 +17,7 @@ type IngestLogEntry struct {
 	Timestamp      string            `json:"timestamp" validate:"required,datetime=2006-01-02T15:04:05Z07:00"`
 	Level          string            `json:"level" validate:"required,oneof=INFO WARN ERROR DEBUG"`
 	Message        string            `json:"message" validate:"required"`
-	UserID         string            `json:"uesrId" validate:"omitempty,uuid4"`
+	UserID         string            `json:"userId" validate:"omitempty,uuid4"`
 	AdditionalData map[string]string `json:"additionalData" validate:"omitempty"`
 }
 
@@ -25,8 +25,8 @@ type ProccessLogEntry struct {
 	Timestamp      string            `json:"timestamp" validate:"required,datetime=2006-01-02T15:04:05Z07:00"`
 	Level          string            `json:"level" validate:"required,oneof=INFO WARN ERROR DEBUG"`
 	Message        string            `json:"message" validate:"required"`
-	UserID         string            `json:"uesrId" validate:"omitempty,uuid4"`
+	UserID         string            `json:"userId" validate:"omitempty,uuid4"`
 	AdditionalData map[string]string `json:"additionalData" validate:"omitempty"`
 
-	Processed bool
+	Processed bool `json:"processed"`
 }
